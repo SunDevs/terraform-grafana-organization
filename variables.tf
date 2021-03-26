a454334394488b27e95a275062c7aec183739fae
Ivan Botero <ivan.botero@sundevs.com>
2021-03-26 15:18:01 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "NAME" {
+  type        = string
+  description = "Organization name."
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_DEFAULT_REGION" {
+  type = string
+}
